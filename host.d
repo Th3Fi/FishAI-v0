3e103c44477eab87dbde30b625aa0cb486c584d0
Th3Fi <156732301+Th3Fi@users.noreply.github.com>
2026-05-24 17:12:29 -0500

    Tokenizer for Fish Super intellegence
    Full rewrite of original program
    Simpler
    Faster
    Better
    also started to work on BPE

--- host.d
+++ host.d
@@ -1,113 +1,55 @@
 import std.algorithm : splitter;
 import std.array : array;
-import std.string : indexOf, indexOfAny;
+import std.string : chomp, indexOf, indexOfAny;
+import std.utf;
+import std.parallelism : task;
 import std.stdio;
 
-struct Statement{
-    static int nextStatementId;
-    int statementId;
-    string sentence;
-    int statementType;
+//makes words
+auto tokenMake(string input){
+    auto output = input.splitter(" ").array;
+    return output;
 }
 
-struct Neurons{
-    double[3] neurons = [1.0, 2.0, 3.0];
-    double[3] weights1 = [0.1, 0.3, 0.5];
-    double[3] weights2 = [0.2, 0.4 , 0.6];
-    double[2] bias = [0.1 , 0.2];
-}
-
-
-//paragraph functions
-
-Statement makeSentence(string sentenceGen, int sentenceType){
-
-    //Statement ID assignment
-    int statementId = Statement.nextStatementId;
-    ++Statement.nextStatementId;
-
-    //bob the builder
-    return Statement(statementId, sentenceGen, sentenceType);
-}
-
-
-string slicer(string paragraph, string findChar, string lastSentence){
-
-    //Initializes a new paragraph using lastSentence
-    string newParagraph = paragraph[lastSentence.length..paragraph.length];
-    //Find a sentence from paragraph using the length of the slice newParagraph to find the next punctuation
-    string sentenceGen = newParagraph[0..(newParagraph.indexOfAny(findChar)+1)];
-
-    return sentenceGen;
-}
-
-
-int sliceContext(string sentenceGen){
-    //Sentence type assignment (used for analyzing the context and creating an inference)
-    int sentenceType;
+// makes words into utf-8
+void tokenEncode(string input, int workerId){
+    int[] output;
     {
-    auto statement = sentenceGen.indexOf("!");
-    sentenceType = (statement != -1) ? 1 : 0;
-    }
-
-    {
-    auto statement = sentenceGen.indexOf("?");
-    sentenceType = (statement != -1) ? 2 : 0;
-    }
-    return (sentenceType);
-}
-
-// sentence functions
-
-double tokenize(string statement){
-
-}
-
-
-
-void main() {
-    Statement[] statements;
-    {
-        string statement = readln();
-        string statementContext = ".!?";
-        string[] paragraph;
-        string lastSentence; //used for slicer calls to be used in a for loop.
-
-
-        writeln(statement.length);
-        for(int i; (lastSentence.length + 1) != statement.length ; ++i){
-            writeln(lastSentence.length); // debug lines for testing, uncomment to debug
-            writeln(slicer(statement, statementContext, lastSentence));
-            lastSentence ~= slicer(statement, statementContext, lastSentence);
-            paragraph.length += 1;
-            paragraph[i] = lastSentence;
-        }
-
-
         int i;
-            {
-            foreach(num; paragraph){
-                auto sentence = makeSentence(num,(sliceContext(num)));
-                statements.length += 1;
-                statements[i] = sentence;
-                ++i;
+        foreach(c; input.byUTF!char){
+            ++output.length;
+            output[i] = c;
+            ++i;
+        }
+    }
+    writeln(workerId, " | ", input, " | ", output[]); // test worker outputs
+
+    //detect pairs (ballz, haha he said balls in the comments)
+    {
+        int[] mergePair;
+        for(int i = 0; i != ((output.length) - 1) ; ++i){
+        writeln(workerId, " | oper : ", output[i], " ", output[i+1] , " | paired : ", mergePair[]);
+            if(output[i] == output[i + 1]){
+                mergePair ~= output[i] + output[i+1];
             }
-        writeln(statements);
+        }
+    }
+}
 
+void main(){
+    //makes a string array automatically now.
+    string input = chomp(readln()); //uses chomp to remove /n
+    writeln(tokenMake(input));
+    auto words = (tokenMake(input));
 
+    // half this code was written on my headset with a 3.5in SPI display, get on my level
+    { // prevents count duku from escaping your 2GBs of cloud DDR at AWS
+        int count;
+        foreach(word; words){
+            auto worker = task!tokenEncode(word, count);
+            worker.executeInNewThread();
+            ++count;
         }
     }
 
-
-    /*auto question = text.indexOfAny(punctuation);
-
-    writeln(question);
-    string delimiters = " ";
-
-    auto tokens = text.splitter(delimiters);
-
-    foreach(token; tokens){
-        writeln(token);
-    }
-    */
 }
